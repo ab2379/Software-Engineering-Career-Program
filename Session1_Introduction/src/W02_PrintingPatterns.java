165ea314f36c3a5da68c91100449ea8f3af9cd10
ab2379 <ab2379@github.com>
2023-10-28 09:59:47 +0100

    Session 1

---
+++ Session1_Introduction/src/W02_PrintingPatterns.java
@@ -0,0 +1,17 @@
+public class W02_PrintingPatterns {
+
+    public static void trianglePattern1(int n){
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++) {
+                count++;
+                System.out.print(count + "* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        trianglePattern1(5);
+    }
+}
