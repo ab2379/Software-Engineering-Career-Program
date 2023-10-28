5eb1737015fbf270549142689873be883b83405d
ab2379 <ab2379@github.com>
2023-10-28 12:51:30 +0100

    Session 1 with pom.xml

--- Session1/src/test/java/CalculatorTest.java
+++ Session1/src/test/java/CalculatorTest.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class CalculatorTest {
+import org.example.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 4);
+
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void testSubstract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.substract(3, 4);
+
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(3, 4);
+
+        assertEquals(12, result);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        //int result = calculator.divide(8, 4);
+        int result = calculator.divide(8, 0);
+
+        //assertEquals(2, result);
+
+    }
 }
